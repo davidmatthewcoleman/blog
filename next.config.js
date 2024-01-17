182b2a4eae8544b922133b963f0dcacdde9ce30a
davidmc.io <hi@davidmc.io>
2024-01-17 07:50:37 -0600

    Fix CSS.

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
     WORDPRESS_HOST: process.env.WORDPRESS_HOST,
     FRONTEND_HOST: process.env.FRONTEND_HOST,
     IMGIX_HOST: process.env.IMGIX_HOST
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://secure.davidmc.io" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
