f911665939f1efa2c6d25b2d8ea7c63d1303421d
davidmc.io <hi@davidmc.io>
2024-01-18 00:30:31 -0600

    Fix CSS.

--- components/WPAdminBar.tsx
+++ components/WPAdminBar.tsx
@@ -2,21 +2,24 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 function trimSlashes(str: any){
-    str= str.startsWith('/') ? str.substr(1) : str;
-    str= str.endsWith('/') ? str.substr(0,str.length - 1) : str;
+    str = str.startsWith('/') ? str.substr(1) : str;
+    str = str.endsWith('/') ? str.substr(0, str.length - 1) : str;
     return str;
 }
+
 const WPAdminBar = () => {
     const router = useRouter();
     const [adminBarHtml, setAdminBarHtml] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (!router.isReady) return;
 
         const fetchAdminBar = async () => {
+            setIsLoading(true);
             try {
                 let response;
-                if ( router.asPath === '/' || trimSlashes(router.asPath).indexOf('/') !== -1 ) {
+                if (router.asPath === '/' || trimSlashes(router.asPath).indexOf('/') !== -1) {
                     response = await fetch(`/api/getAdminBar`);
                 } else {
                     response = await fetch(`/api/getAdminBar?slug=${router.asPath}`);
@@ -31,10 +34,15 @@ const WPAdminBar = () => {
             } catch (error) {
                 console.error('Error fetching admin bar:', error);
             }
+            setIsLoading(false);
         };
 
         fetchAdminBar();
     }, [router.isReady, router.asPath]);
+
+    if (isLoading) {
+        return <div className={`wp-admin-bar-placeholder`}/>;
+    }
 
     return (
         <div className={`wp-admin-bar-container`} dangerouslySetInnerHTML={{ __html: adminBarHtml }} />
