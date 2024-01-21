08e5931f40ab549d8bb353e2779de8149e7286a9
davidmc.io <hi@davidmc.io>
2024-01-21 06:56:29 -0600

    Fix CSS.

--- components/header.tsx
+++ components/header.tsx
@@ -80,7 +80,7 @@ function Header({menu, options, latestPosts}: {menu: any, options: any, latestPo
     };
 
     return (
-        <header className={`lg:sticky lg:top-0 w-full lg:w-1/3 xl:w-1/4 2xl:w-1/5 ${headerState ? 'closed' : 'open'} overflow-hidden font-sans bg-black/80 h-auto max-h-max xl:h-screen backdrop-blur-md backdrop-saturate-200`}>
+        <header className={`lg:sticky lg:top-0 w-full lg:w-1/3 xl:w-1/4 2xl:w-1/5 ${headerState ? 'closed' : 'open'} overflow-hidden font-sans bg-black/80 h-auto max-h-max lg:h-screen backdrop-blur-md backdrop-saturate-200`}>
             <button onClick={toggleHeader} className={`flex flex-row ${headerState ? 'text-white/50' : 'text-bright-sun-400'} max-w-max pt-7 pb-3 my-0 mx-auto`}>
                 <svg viewBox="0 0 24 24" width={16} height={16} className={`inline-flex fill-current my-1`}>
                     <path d={headerState ? `M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z` : `M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z`} />
