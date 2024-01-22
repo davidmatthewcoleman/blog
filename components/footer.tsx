d576ff473dd2cd6e64558560e2f172ee3ae53d2b
davidmc.io <hi@davidmc.io>
2024-01-22 03:26:37 -0600

    Fix CSS.

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,8 @@ import { useState } from 'react';
 function Footer({loadMore, options}: {loadMore: any, options: any}) {
     return (
         <footer className={`relative flex flex-col md:flex-row gap-3 py-6 px-8 mt-auto text-sm uppercase tracking-wider bg-white border-t border-t-black/10 z-10 font-sans`}>
-            <span className={`hidden xl:inline-flex`}>{options.site_footer.desktop.replace('[y]', format(new Date(), 'yyyy'))}</span>
-            <span className={`md:inline-flex xl:hidden text-center md:text-left`}>{options.site_footer.mobile.replace('[y]', format(new Date(), 'yyyy'))}</span>
+            <span className={`hidden xl:inline-flex`}>{options.site_footer.desktop}</span>
+            <span className={`md:inline-flex xl:hidden text-center md:text-left`}>{options.site_footer.mobile}</span>
             <span
                 className={`mx-auto md:ml-auto md:mr-0`}
             >
