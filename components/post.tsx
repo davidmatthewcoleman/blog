1219a29a699d15135dcc69a5e12c9841abaab9e3
davidmc.io <hi@davidmc.io>
2024-01-22 14:57:23 -0600

    Fix CSS.

--- components/post.tsx
+++ components/post.tsx
@@ -112,9 +112,17 @@ const Post: React.FC<PostProps> = ({ data, single }) => {
                 )}
                 {
                     data.sticky ? (
-                        <svg viewBox="0 0 24 24" width={20} height={20}
-                             className={`absolute inset-0 top-auto ${data.subtitle ? 'mb-9' : ( single ? 'mb-11' : 'mb-10' ) } mx-[14px] xl:mx-[22px] fill-current scale-125 z-30`}><path d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z" />
-                        </svg>
+                        single ? (
+                            <svg viewBox="0 0 20 20" width={20} height={20}
+                                 className={`absolute inset-0 top-auto ${data.subtitle ? 'mb-9' : ( single ? 'mb-11' : 'mb-10' ) } mx-[14px] xl:mx-[22px] fill-current z-30`}>
+                                <path
+                                    d={data.type === 'post' ? 'M18 3v2H2V3h16zm-6 4v2H2V7h10zm6 0v2h-4V7h4zM8 11v2H2v-2h6zm10 0v2h-8v-2h8zm-4 4v2H2v-2h12z' : 'M3 1v18h14V1H3zm9 13H6v-1h6v1zm2-3H6v-1h8v1zm0-3H6V7h8v1zm0-3H6V4h8v1z'}/>
+                            </svg>
+                        ) : (
+                            <svg viewBox="0 0 24 24" width={20} height={20}
+                                 className={`absolute inset-0 top-auto ${data.subtitle ? 'mb-9' : ( single ? 'mb-11' : 'mb-10' ) } mx-[14px] xl:mx-[22px] fill-current scale-125 z-30`}><path d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z" />
+                            </svg>
+                        )
                     ) : (
                         <svg viewBox="0 0 20 20" width={20} height={20}
                              className={`absolute inset-0 top-auto ${data.subtitle ? 'mb-9' : ( single ? 'mb-11' : 'mb-10' ) } mx-[14px] xl:mx-[22px] fill-current z-30`}>
