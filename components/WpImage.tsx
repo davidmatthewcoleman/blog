dbf5e1f349e7042bfa5eebb7e58356cbad5b52b0
davidmc.io <hi@davidmc.io>
2024-01-25 02:47:25 -0600

    Fix CSS.

--- components/WpImage.tsx
+++ components/WpImage.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import dynamic from "next/dynamic";
 
 // Function to create a placeholder with a dynamic className
-const createPlaceholder = (src: any, className: any) => () => (
-    <div className={className} style={{ width: (Object.values(src).pop() as any)[0].width, height: (Object.values(src).pop() as any)[0].height }}></div>
-);
+const createPlaceholder = (src: any, className: any) => {
+    const Placeholder = () => (
+        <div className={className} style={{ width: (Object.values(src).pop() as any)[0].width, height: (Object.values(src).pop() as any)[0].height }}></div>
+    );
+    Placeholder.displayName = 'Placeholder'; // Set displayName for the Placeholder component
+    return Placeholder;
+};
 
 const WpImage = ({ url, src, className, alt, focalPoint, props }: { url: string, src: any, className: string, alt: string, focalPoint: any, props: any }) => {
     // Dynamically import GetImage with a className-aware placeholder
@@ -12,6 +16,8 @@ const WpImage = ({ url, src, className, alt, focalPoint, props }: { url: string,
         ssr: true,
         loading: createPlaceholder(src, className),
     });
+
+    GetImage.displayName = 'GetImage';
 
     return <GetImage url={url} src={src} className={className} alt={alt} focalPoint={focalPoint} {...props} />;
 }
